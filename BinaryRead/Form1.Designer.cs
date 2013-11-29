5f93014dbf65d38f6296d9f6d06379ead1918a65
Kerem VARIŞ <webyonett@gmail.com>
2013-11-29 13:58:32 +0200

    MsSql yapısında binary olarak kaydedilen dökümanları okuyup tekrar yaratan bir fonksiyonu vardır

---
+++ BinaryRead/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BinaryRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.dokumannoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.revizyonnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.docdokumandoczipBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.isoft9000DataSet = new BinaryRead.isoft9000DataSet();
+			this.doc_dokuman_doczipTableAdapter = new BinaryRead.isoft9000DataSetTableAdapters.doc_dokuman_doczipTableAdapter();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.docdokumandoczipBindingSource)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.isoft9000DataSet)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(0, 0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoGenerateColumns = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dokumannoDataGridViewTextBoxColumn,
+            this.revizyonnoDataGridViewTextBoxColumn});
+			this.dataGridView1.DataSource = this.docdokumandoczipBindingSource;
+			this.dataGridView1.Location = new System.Drawing.Point(59, 100);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(438, 150);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// dokumannoDataGridViewTextBoxColumn
+			// 
+			this.dokumannoDataGridViewTextBoxColumn.DataPropertyName = "dokuman_no";
+			this.dokumannoDataGridViewTextBoxColumn.HeaderText = "dokuman_no";
+			this.dokumannoDataGridViewTextBoxColumn.Name = "dokumannoDataGridViewTextBoxColumn";
+			// 
+			// revizyonnoDataGridViewTextBoxColumn
+			// 
+			this.revizyonnoDataGridViewTextBoxColumn.DataPropertyName = "revizyon_no";
+			this.revizyonnoDataGridViewTextBoxColumn.HeaderText = "revizyon_no";
+			this.revizyonnoDataGridViewTextBoxColumn.Name = "revizyonnoDataGridViewTextBoxColumn";
+			// 
+			// docdokumandoczipBindingSource
+			// 
+			this.docdokumandoczipBindingSource.DataMember = "doc_dokuman_doczip";
+			this.docdokumandoczipBindingSource.DataSource = this.isoft9000DataSet;
+			// 
+			// isoft9000DataSet
+			// 
+			this.isoft9000DataSet.DataSetName = "isoft9000DataSet";
+			this.isoft9000DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+			// 
+			// doc_dokuman_doczipTableAdapter
+			// 
+			this.doc_dokuman_doczipTableAdapter.ClearBeforeFill = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(90, 0);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(542, 337);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.docdokumandoczipBindingSource)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.isoft9000DataSet)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private isoft9000DataSet isoft9000DataSet;
+        private System.Windows.Forms.BindingSource docdokumandoczipBindingSource;
+        private isoft9000DataSetTableAdapters.doc_dokuman_doczipTableAdapter doc_dokuman_doczipTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dokumannoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn revizyonnoDataGridViewTextBoxColumn;
+				private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BinaryRead/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace BinaryRead
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'isoft9000DataSet.doc_dokuman_doczip' table. You can move, or remove it, as needed.
+            this.doc_dokuman_doczipTableAdapter.Fill(this.isoft9000DataSet.doc_dokuman_doczip);
+
+        }
+        public static string Temizle(string Metin)
+        {
+            string deger = Metin;
+            
+            deger = deger.Replace(" ", "_");
+            deger = deger.Replace("/", "_");
+            deger = deger.Replace(">", "");
+            deger = deger.Replace("&", "");
+            deger = deger.Replace("[", "");
+            deger = deger.Replace("]", "");
+            deger = deger.Replace("--", "");
+            deger = deger.Replace(";", "");
+
+            return deger;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=NEPTUN\DVLP2008;Initial Catalog=DocumentLibrary;Integrated Security=True");
+            SqlDataAdapter da = new SqlDataAdapter(@"SELECT big.doc_dokuman.id, 
+big.doc_dokuman.dokuman_adi,
+big.doc_dokuman_doczip.dokuman_no, 
+big.doc_dokuman_doczip.zip 
+FROM big.doc_dokuman_doczip
+inner join 
+big.doc_dokuman on 
+big.doc_dokuman.dokuman_no=big.doc_dokuman_doczip.dokuman_no", conn);
+            var t = new DataTable();
+            da.Fill(t);
+            foreach (DataRow row in t.Rows)
+            {
+                //var row = t.Rows[0];
+                byte[] bytes = (byte[])row["zip"];
+                string fname = (string)row["dokuman_no"] + "_" + (string)row["dokuman_adi"];
+                fname = fname.Trim();
+                File.WriteAllBytes(string.Format(@"c:\dosyalar\{0}.zip", Temizle(fname)), bytes);
+            }
+        }
+
+				private void button2_Click(object sender, EventArgs e)
+				{
+					SqlConnection conn = new SqlConnection(@"Data Source=NEPTUN\DVLP2008;Initial Catalog=isoft9000;Integrated Security=True");
+					SqlDataAdapter da = new SqlDataAdapter(@"SELECT 
+big.doc_formlar.ad,
+big.doc_formlar_webzip.form_no, 
+big.doc_formlar_webzip.form
+
+FROM big.doc_formlar_webzip
+inner join 
+big.doc_formlar on 
+big.doc_formlar.form_no=big.doc_formlar_webzip.form_no", conn);
+					var t = new DataTable();
+					da.Fill(t);
+					foreach (DataRow row in t.Rows)
+					{
+						//var row = t.Rows[0];
+						byte[] bytes = (byte[])row["form"];
+						string fname = (string)row["form_no"] + "_" + (string)row["ad"];
+						fname = fname.Trim();
+						File.WriteAllBytes(string.Format(@"c:\formlar\{0}.zip", Temizle(fname)), bytes);
+					}
+				}
+    }
+}
